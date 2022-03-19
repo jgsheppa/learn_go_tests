3dfa2f9422216d6e552a020d7008989f82d3dbb5
jgsheppa <james.sheppard@avisio.ai>
2022-03-19 21:53:08 +0100

    Add to Hello function and expand test

--- hello.go
+++ hello.go
@@ -2,11 +2,34 @@ package main
 
 import "fmt"
 
-func Hello(name string) string {
-	return "Hello, " + name
+const (
+	spanish = "ES"
+	french = "FR"
+	german = "DE"
+	englishHelloPrefix = "Hello, "
+ 	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix = "Bonjour, "
+	germanHelloPrefix = "Hallo, "
+)
+
+func Hello(name, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	switch language {
+	case spanish:
+		return spanishHelloPrefix + name
+	case french:
+		return frenchHelloPrefix + name
+	case german:
+		return germanHelloPrefix + name
+	default:
+		return englishHelloPrefix + name
+	}
 }
 
 
 func main() {
-	fmt.Println(Hello("there"))
+	fmt.Println(Hello("there", "EN"))
 }
--- hello_test.go
+++ hello_test.go
@@ -3,10 +3,36 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("there")
-	want := "Hello, there"
-
-	if got != want {
-		t.Errorf("Got %q want %q", got, want)
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Maria", "ES")
+		want := "Hola, Maria"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in German", func(t *testing.T) {
+		got := Hello("Franz", "DE")
+		want := "Hallo, Franz"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Zoé", "FR")
+		want := "Bonjour, Zoé"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("saying hello to someone, something, or nothing", func(t *testing.T) {
+		got := Hello("there", "EN")
+		want := "Hello, there"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("", "EN")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
 }
