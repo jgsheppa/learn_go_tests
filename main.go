6e801bdd66f504e6afad6b49dcc9d06139131fa4
jgsheppa <james.sheppard@avisio.ai>
2022-03-24 23:37:26 +0100

    Write tests for injection

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 
 	"github.com/jgsheppa/learn_go_tests/hello"
+	"github.com/jgsheppa/learn_go_tests/injection"
 	"github.com/jgsheppa/learn_go_tests/integer"
 	"github.com/jgsheppa/learn_go_tests/iteration"
 )
@@ -12,4 +15,5 @@ func main() {
 	fmt.Println(hello.Hello("there", "EN"))
 	fmt.Println(integer.Add(3, 3))
 	fmt.Println(iteration.Repeat("hey", 5))
+	log.Fatal(http.ListenAndServe(":5000", http.HandlerFunc(injection.MyGreeterHandler)))
 }
